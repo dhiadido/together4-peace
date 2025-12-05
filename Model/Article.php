0f3db4f1bbab0afc39e48ad624f0321c8e54fe7e
Gaston007 <ghassen.tounsi2@gmail.com>
2025-12-05 05:11:51 +0100

    Add Model folder with Article and Offres classes

---
+++ Model/Article.php
@@ -0,0 +1,74 @@
+<?php
+
+class Article {
+    private $id;
+    private $titre;
+    private $theme;
+    private $resume;
+    private $contenu;
+    private $image;
+
+    // Constructor
+    public function __construct($titre = null, $theme = null, $resume = null, 
+                                $contenu = null, 
+                                $image = '../../assets/images/logo.png') 
+    {
+        $this->titre = $titre;
+        $this->theme = $theme;
+        $this->resume = $resume;
+        $this->contenu = $contenu;
+        $this->image = $image;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitre() {
+        return $this->titre;
+    }
+
+    public function getTheme() {
+        return $this->theme;
+    }
+
+    public function getResume() {
+        return $this->resume;
+    }
+
+    public function getContenu() {
+        return $this->contenu;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setTitre($titre) {
+        $this->titre = $titre;
+    }
+
+    public function setTheme($theme) {
+        $this->theme = $theme;
+    }
+
+    public function setResume($resume) {
+        $this->resume = $resume;
+    }
+
+    public function setContenu($contenu) {
+        $this->contenu = $contenu;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+}
+
+?>
---
+++ Model/Offres.php
@@ -0,0 +1,103 @@
+<?php
+
+class Offre {
+    private $id;
+    private $nomSpecialiste;
+    private $description;
+    private $prix;
+    private $categorie;
+    private $categorieProbleme;
+    private $contact;
+    private $image;
+    private $article_id; // CHANGED
+
+    // Constructor
+    public function __construct($nomSpecialiste = null, $description = null, $prix = null, 
+                                $categorie = null, $categorieProbleme = null, $contact = null, 
+                                $image = '../../assets/images/logo.png', $article_id = null) { // CHANGED
+        $this->nomSpecialiste = $nomSpecialiste;
+        $this->description = $description;
+        $this->prix = $prix;
+        $this->categorie = $categorie;
+        $this->categorieProbleme = $categorieProbleme;
+        $this->contact = $contact;
+        $this->image = $image;
+        $this->article_id = $article_id; // CHANGED
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNomSpecialiste() {
+        return $this->nomSpecialiste;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getPrix() {
+        return $this->prix;
+    }
+
+    public function getCategorie() {
+        return $this->categorie;
+    }
+
+    public function getCategorieProbleme() {
+        return $this->categorieProbleme;
+    }
+
+    public function getContact() {
+        return $this->contact;
+    }
+
+    public function getImage() {
+        return $this->image;
+    }
+
+    public function getArticleId() { // CHANGED
+        return $this->article_id;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNomSpecialiste($nomSpecialiste) {
+        $this->nomSpecialiste = $nomSpecialiste;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function setPrix($prix) {
+        $this->prix = $prix;
+    }
+
+    public function setCategorie($categorie) {
+        $this->categorie = $categorie;
+    }
+
+    public function setCategorieProbleme($categorieProbleme) {
+        $this->categorieProbleme = $categorieProbleme;
+    }
+
+    public function setContact($contact) {
+        $this->contact = $contact;
+    }
+
+    public function setImage($image) {
+        $this->image = $image;
+    }
+
+    public function setArticleId($article_id) { // CHANGED
+        $this->article_id = $article_id;
+    }
+}
+
+?>
